163e0cfb96d9225ef9dc73ab48e90072bc0d75f2
hyoseok <alexization@gmail.com>
2024-11-16 13:58:52 +0900

    init: Setting up the initial server environment

---
+++ src/main/java/ddog/mungleserver/MungleServerApplication.java
@@ -0,0 +1,13 @@
+package ddog.mungleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MungleServerApplication.class, args);
+    }
+
+}
