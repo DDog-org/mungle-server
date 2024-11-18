724fdec43c377971266e767cf058d89d6c56cad4
hyoseok <alexization@gmail.com>
2024-11-18 16:48:11 +0900

    [feat/mungle-1] Change Project name

--- src/main/java/ddog/mungleserver/MeongleServerApplication.java
+++ src/main/java/ddog/mungleserver/MeongleServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MungleServerApplication {
+public class MeongleServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MungleServerApplication.class, args);
+        SpringApplication.run(MeongleServerApplication.class, args);
     }
 
 }
