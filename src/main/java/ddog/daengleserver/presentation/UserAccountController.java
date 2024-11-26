0f4c05363e70ae3509c740d017d05882f7abb54b
hyoseok <alexization@gmail.com>
2024-11-26 23:48:09 +0900

    [refactoring/daengle-19] 컨트롤러명 변경 및 중복검사 엔드포인트 변경

--- src/main/java/ddog/daengleserver/presentation/UserAccountController.java
+++ src/main/java/ddog/daengleserver/presentation/UserAccountController.java
@@ -15,24 +15,24 @@ import static ddog.daengleserver.global.common.CommonResponseEntity.success;
 import static ddog.daengleserver.presentation.enums.JoinControllerResp.USER_JOIN_COMPLETED;
 
 @RestController
-@RequestMapping("/api/join")
+@RequestMapping("/api/daengle")
 @RequiredArgsConstructor
-public class JoinController {
+public class UserAccountController {
 
     private final UserService userService;
 
-    @PostMapping("/nickname")
+    @PostMapping("/available-nickname")
     public CommonResponseEntity<Boolean> hasNickname(@RequestBody CheckNicknameReq request) {
         return success(userService.hasNickname(request.getNickname()));
     }
 
-    @PostMapping("/daengle-without-pet")
+    @PostMapping("/join-without-pet")
     public CommonResponseEntity<String> joinUserWithoutPet(@RequestBody JoinUserWithoutPet request) {
         userService.createUserWithoutPet(request);
         return success(USER_JOIN_COMPLETED.getMessage());
     }
 
-    @PostMapping("/daengle-with-pet")
+    @PostMapping("/join-with-pet")
     public CommonResponseEntity<String> joinUserWithPet(@RequestBody JoinUserWithPet request) {
         userService.createUserWithPet(request);
         return success(USER_JOIN_COMPLETED.getMessage());
