8fdbcef99a16cc5c3101c1be32d441de8c6f1c03
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-11-26 19:47:01 +0900

    [feat/daengle-11] Test Code gradle 수정

--- src/test/java/ddog/daengleserver/DaengleServerApplicationTests.java
+++ src/test/java/ddog/daengleserver/DaengleServerApplicationTests.java
@@ -3,7 +3,7 @@ package ddog.daengleserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DaengleServerApplicationTests.class)
+@SpringBootTest
 class DaengleServerApplicationTests {
 
     @Test
