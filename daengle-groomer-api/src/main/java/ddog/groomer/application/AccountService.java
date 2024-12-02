8bb0d402c9d65881163ee6896a80319eafecf798
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-12-03 04:09:32 +0900

    [refactoring/daengle-35] 미용사 등록 요청 데이터 포멧 유효성 검사 로직 구현

--- daengle-groomer-api/src/main/java/ddog/groomer/application/AccountService.java
+++ daengle-groomer-api/src/main/java/ddog/groomer/application/AccountService.java
@@ -4,6 +4,8 @@ import ddog.auth.config.jwt.JwtTokenProvider;
 import ddog.domain.account.Account;
 import ddog.domain.account.Role;
 import ddog.domain.groomer.Groomer;
+import ddog.groomer.application.exception.AccountException;
+import ddog.groomer.application.exception.AccountExceptionType;
 import ddog.groomer.application.mapper.GroomerMapper;
 import ddog.groomer.presentation.account.dto.ModifyInfoReq;
 import ddog.groomer.presentation.account.dto.ProfileInfo;
@@ -13,6 +15,7 @@ import ddog.persistence.port.AccountPersist;
 import ddog.persistence.port.GroomerPersist;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -24,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccountService {
@@ -34,6 +38,10 @@ public class AccountService {
 
     @Transactional
     public SignUpResp signUp(SignUpReq request, HttpServletResponse response) {
+        if (this.hasInvalidGroomerDataFormat(request)) {
+            throw new AccountException(AccountExceptionType.INVALID_REQUEST_DATA_FORMAT);
+        }
+
         Account newAccount = Account.create(request.getEmail(), Role.GROOMER);
         Account savedAccount = accountPersist.save(newAccount);
 
@@ -69,5 +77,27 @@ public class AccountService {
         Groomer groomer = groomerPersist.getGroomerByAccountId(accountId);
         Groomer updatedGroomer = GroomerMapper.withUpdate(groomer, request);
         groomerPersist.save(updatedGroomer);
+    }
+
+    private boolean hasInvalidGroomerDataFormat(SignUpReq request) {
+        try {
+            Groomer.validateShopName(request.getShopName());
+            log.info("1");
+            Groomer.validateName(request.getName());
+            log.info("2");
+            Groomer.validatePhoneNumber(request.getPhoneNumber());
+            log.info("3");
+            Groomer.validateAddress(request.getAddress());
+            log.info("4");
+            Groomer.validateDetailAddress(request.getDetailAddress());
+            log.info("5");
+            Groomer.validateBusinessLicenses(request.getBusinessLicenses());
+            log.info("6");
+            Groomer.validateLicenses(request.getLicenses());
+
+            return false; // 모든 유효성 검사 통과
+        } catch (IllegalArgumentException e) {
+            return true; // 유효성 검사 실패
+        }
     }
 }
