c6009fd98999ce1f4aa8ba139a6ca3af03710739
hyoseok <alexization@gmail.com>
2024-12-02 22:10:49 +0900

    [feat/daengle-32] 미용사 refreshToken 로직 구현

--- daengle-payment-api/src/main/java/ddog/payment/application/exception/common/AuthException.java
+++
@@ -1,7 +0,0 @@
-package ddog.payment.application.exception.common;
-
-public class AuthException extends CustomRuntimeException {
-    public AuthException(AuthExceptionType type, Object... args) {
-        super(type.getMessage(), type.getHttpStatus(), type.getCode());
-    }
-}
