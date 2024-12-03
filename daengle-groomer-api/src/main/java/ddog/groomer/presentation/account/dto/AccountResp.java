963703b34c95e2f39f2c9f820e61342748cd5f4a
hyoseok <alexization@gmail.com>
2024-12-04 03:21:41 +0900

    [refactoring/daengle-36] 미용사 프로필 관련 유효성 검증 로직 리팩토링

---
+++ daengle-groomer-api/src/main/java/ddog/groomer/presentation/account/dto/AccountResp.java
@@ -0,0 +1,10 @@
+package ddog.groomer.presentation.account.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AccountResp {
+    private String requestResult;
+}
