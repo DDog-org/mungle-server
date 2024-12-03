55ff0af670898bce66ffa113475ad91f19e85c27
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-12-03 17:49:13 +0900

    [feat/daengle-29] 채팅 관련 기본 설계

--- daengle-domain/src/main/java/ddog/domain/chat/ChatMessage.java
+++ daengle-domain/src/main/java/ddog/domain/chat/ChatMessage.java
@@ -6,15 +6,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatMessage {
 
+    private Long chatRoomId;
     private Long messageId;
     private ChatType messageType; // 기본 메시지 or 사진
     private Long senderId; // 보내는 사람
     private Long recipientId; // 받는 사람
+    private LocalDateTime timestamp;
 
 }
