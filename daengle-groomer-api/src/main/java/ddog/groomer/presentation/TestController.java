17ff3cf5f60e095cbe6e9a54264e8a24b3b7e016
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-12-08 15:30:00 +0900

    [refactoring/daengle-45] 테스트를 위한 CD 설정 일부 변경

--- daengle-groomer-api/src/main/java/ddog/groomer/presentation/TestController.java
+++ daengle-groomer-api/src/main/java/ddog/groomer/presentation/TestController.java
@@ -1,5 +1,6 @@
 package ddog.groomer.presentation;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,22 @@ import java.time.format.DateTimeFormatter;
 @RestController
 @RequestMapping("/api/groomer")
 public class TestController {
-    @GetMapping("/test")
-    public String test() {
+    private String deploymentTime;
+
+    // 애플리케이션 시작 시 배포 시간을 기록
+    @PostConstruct
+    public void init() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedDate = now.format(formatter);
-        return "Hello Daengle World - MULTI MODULE !!!! Groomer API !" +
-                " Made at: " + formattedDate + "   CI/CD SUCCESS";
+        deploymentTime = now.format(formatter);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+//        LocalDateTime now = LocalDateTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        String formattedDate = now.format(formatter);
+        return "Hello Daengle World - MULTI MODULE !!!! GROOMER API !" +
+                " Made at: " + deploymentTime + "   CI/CD SUCCESS";
     }
 }
