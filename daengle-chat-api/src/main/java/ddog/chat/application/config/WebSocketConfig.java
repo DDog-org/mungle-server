9c897c5224565b6632818198a57955b58d556ea0
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-12-16 03:50:53 +0900

    [feat/daengle-46] websocket 연결 설정

--- daengle-chat-api/src/main/java/ddog/chat/application/config/WebSocketConfig.java
+++ daengle-chat-api/src/main/java/ddog/chat/application/config/WebSocketConfig.java
@@ -15,8 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+            .setAllowedOriginPatterns("*");
     }
 
     @Override
