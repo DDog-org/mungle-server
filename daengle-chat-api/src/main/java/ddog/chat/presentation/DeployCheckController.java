b2e7feb27c81366c597d92e582c2ecd08e6d8f63
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-12-16 02:02:36 +0900

    [feat/daengle-46] 배포 테스트 컨트롤러 수정

--- daengle-chat-api/src/main/java/ddog/chat/presentation/DeployCheckController.java
+++ daengle-chat-api/src/main/java/ddog/chat/presentation/DeployCheckController.java
@@ -26,7 +26,7 @@ public class DeployCheckController {
 //        LocalDateTime now = LocalDateTime.now();
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 //        String formattedDate = now.format(formatter);
-        return "Hello Daengle World - MULTI MODULE !!!! GROOMER API !" +
+        return "Hello Daengle World - MULTI MODULE !!!! CHAT API !" +
                 " Made at: " + deploymentTime + "   CI/CD SUCCESS";
     }
 }
